0f2f91ac0ca0b639c68c6c1842c8b7be8d6bdc2a
begging-pp0 <pp0begging@gmail.com>
2024-08-18 01:17:57 +0000

    initial application source code

---
+++ src/test/java/com/kk/DemowarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kk.demowar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
